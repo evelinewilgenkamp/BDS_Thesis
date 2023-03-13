9d4cec9149cc8e617651e4aa361611a6386cdb27
evelinewilgenkamp <evelinewilgenkamp@hotmail.com>
2023-03-13 11:37:41 +0100

    Create ReplicationDataSetup.py

---
+++ ReplicationDataSetup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+ReplicationDataSetup.py
+
+Purpose:
+    Scrape all data necessary to replicate the paper by Jensen, Kelly,
+    Malamud, & Pedersen (2022)
+
+Version:
+    1. Initialization
+Date:
+    13-03-2023
+
+Author:
+    Eveline Wilgenkamp
+"""
+
+### Stock returns
+# CRSP
+
+
+### Fundamentals
+# Compustat
+
+
+### Anomalies
+# JKP factors
